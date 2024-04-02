55a7b7d63dee3d706a1f87c210e601010d10489a
Andrew <Andrewyhchon@gmail.com>
2024-04-02 15:47:12 -0700

    added in profile picture

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import { Container, Box, Image, Heading } from '@chakra-ui/react'
 const Page = () => {
     return (
         <Container> 
@@ -10,7 +10,23 @@ const Page = () => {
                     <Heading as = "h2" variant = "page-title"> 
                         Andrew Chon
                     </Heading>
-                    <p> Software Developer</p>
+                    <p> Software Developer </p>
+                </Box>
+                <Box 
+                    flexShrink = {0} 
+                    mt = {{ base: 4, md: 0 }} 
+                    ml = {{ md: 6 }} 
+                    align = "center"
+                >
+                    <Image borderColor = "blackAlpha.800" 
+                        borderWidth = {2} 
+                        borderStyle = "solid" 
+                        maxWidth = "100px" 
+                        display = "inline-block" 
+                        borderRadius = "full"
+                        src = "/images/Profile.jpg" 
+                        alt = "Profile Image" 
+                    />
                 </Box>
             </Box>
         </Container>
