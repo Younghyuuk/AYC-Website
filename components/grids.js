99e96df2b5debc92fe4cb45e0f25b92982895732
Andrew <Andrewyhchon@gmail.com>
2024-04-03 23:40:11 -0700

    added in more to the works page

---
+++ components/grids.js
@@ -0,0 +1,59 @@
+import NextLink from 'next/link'
+import Image from 'next/image'
+import { Box, Text, LinkBox, LinkOverlay, Link } from '@chakra-ui/react'
+import { Global } from '@emotion/react'
+
+export const GridItem = ({ children, href, title, thumbnail }) => (
+    <Box w = "100%" align = "center">
+        <LinkBox cursor = "pointer">
+            <Image src = {thumbnail} 
+            alt = {title} 
+            className = "grid-item-thumbnail"
+            placeholder = 'blur'
+            loading = 'lazy'
+            />
+            <LinkOverlay href = {href} target = "_blank">
+                <Text mt = {2}>
+                    {title}
+                </Text>
+            </LinkOverlay>
+            <Text fontSize = {14}> {children} </Text>
+        </LinkBox>
+    </Box>
+
+)
+
+export const WorkGrid = ({ children, id, title, thumbnail }) => (
+    <Box w = "100%" align = "center">
+        <NextLink href = {`/works/${id}`}>
+            <LinkBox cursor = "pointer">
+                <Image src = {thumbnail} alt={title} 
+                className = "grid-item-thumbnail" 
+                placeholder = 'blur' 
+                loading = 'lazy' />
+                <LinkOverlay href = {`/works/${id}`}>
+                    <Text mt = {2} fontSize = {20}>
+                        {title}
+                    </Text>
+                </LinkOverlay>
+                <Text fontSize = {14}> 
+                    {children} 
+                </Text>
+            </LinkBox>
+        </NextLink>
+    </Box>
+)
+
+export const GridItemStyle = () => (
+    <Global styles = {`
+        .grid-item-thumbnail {
+            border-radius: 10px;
+            transition: transform 0.2s;
+        }
+        .grid-item-thumbnail:hover {
+            transform: scale(1.05);
+        }
+
+    `}
+    />
+)
--- pages/works.js
+++ pages/works.js
@@ -1,22 +1,33 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
+import { WorkGrid } from '../components/grids'
+import thumbInkDrop from '../public/images/mouse.png'
+import thumbSlimeDrop from '../public/images/mouse.png'
+import thumbNookDrop from '../public/images/mouse.png'
+import thumbDungeonDrop from '../public/images/mouse.png'
 
-const Works = () => {
-    return (
+const Works = () => (
         <Container>
             <Section>
                 <Heading as = "h3" fontSize = {20} mb = {4}>
                     Works
                 </Heading>
-                {/* <Divider />
-                <SimpleGrid columns = {1} spacing = {4}>
-                    <div>
-                        <p>Here are some of the projects I have worked on:</p>
-                    </div>
-                </SimpleGrid> */}
+                <SimpleGrid columns = {[1,1,2]} gap = {6}>
+                   <Section>
+                    <WorkGrid id = "inkdrop" title = "Inkdrop" thumbnail = {thumbInkDrop}>
+                        A 2d roguelike game made with JavaScript, HTML, and CSS. 
+                        Find your way through hordes of mobs and find the best weapons to stay alive!
+                    </WorkGrid>
+                    </Section> 
+                    <Section>
+                        <WorkGrid id = "FocusNook" title = "FocusNook" thumbnail = {thumbInkDrop}>
+                            A task management tool that enhances productivity.
+                        </WorkGrid>
+                    </Section>
+                </SimpleGrid>
             </Section>
         </Container>
-    )
-}
+)
+
 
 export default Works
