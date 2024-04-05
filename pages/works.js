b96181fd8aaafaeb1646a9ff04ebdd8a44835df4
Andrew <Andrewyhchon@gmail.com>
2024-04-04 21:13:28 -0700

    added in proper sizing for focusNook

--- pages/works.js
+++ pages/works.js
@@ -1,7 +1,6 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
 import { WorkGrid } from '../components/grids'
-import thumbInkDrop from '../public/images/mouse.png'
 import thumbSlime from '../public/images/thumbSlime3.png'
 import thumbNook from '../public/images/thumbNook.png'
 import thumbDungeonDrop from '../public/images/mouse.png'
@@ -13,13 +12,13 @@ const Works = () => (
                     Projects
                 </Heading>
                 <SimpleGrid columns = {[1,1,2]} gap = {6}>
-                   <Section>
+                   <Section delay={0.3}>
                     <WorkGrid id = "inkdrop" title = "Revenge of Slime" thumbnail = {thumbSlime}>
-                        A 2d roguelike game made with JavaScript, HTML, and CSS. 
-                        Find your way through hordes of mobs and find the best weapons to stay alive!
+                        A 2d roguelike game that a team of 3 developers created in 7 weeks.
                     </WorkGrid>
                     </Section> 
-                    <Section>
+                    <Section delay = {0.6}>
+                        {/* <Divider my = {6}/> */}
                         <WorkGrid id = "FocusNook" title = "FocusNook" thumbnail = {thumbNook}>
                             A task management tool that enhances productivity.
                         </WorkGrid>
