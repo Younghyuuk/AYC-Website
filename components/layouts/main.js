da05c6227c5e9a1ec9cd773888fb5af642d05314
Andrew <Andrewyhchon@gmail.com>
2024-04-04 22:49:56 -0700

    added in the image for rotating

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Navbar from '../navbar'
 import { Box, Container }  from '@chakra-ui/react' 
+import LogoPic from '../logoPic'
 
 const Main = ({children, router }) => {
     return (
@@ -13,6 +14,7 @@ const Main = ({children, router }) => {
             <Navbar path = {router.asPath} />
 
             <Container maxW="container.md" pt={14}>
+                <LogoPic />
                 {children}
             </Container>
         </Box>
