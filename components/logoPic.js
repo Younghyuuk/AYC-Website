60afd45fd8ec0e14ffb9aa91ee4499f1651f9942
Andrew <Andrewyhchon@gmail.com>
2024-04-05 00:29:20 -0700

    added in animation to main logo

--- components/logoPic.js
+++ components/logoPic.js
@@ -1,8 +1,9 @@
 import { Box, Image } from '@chakra-ui/react'
-// import Image from 'next/image'
+import { motion } from 'framer-motion'
+
+const MotionBox = motion(Box)
 
 const LogoPic = () => {
-
   return (
     <Box 
       style={{
@@ -12,16 +13,22 @@ const LogoPic = () => {
         height: "20vh"
       }}
     >
-      <Image borderColor = "whiteAlpha.200" 
-          borderWidth = {2} 
-          borderStyle = "solid" 
-          width = "120"
-          height = "120"
-          display = "inline-block" 
-          borderRadius = "full"
-          src = "/images/mainLogo.png" 
-          alt = "Logo Image" 
-       />
+      <MotionBox 
+        animate={{ scale: [1, 1.05, 1] }}
+        transition={{ duration: 2, repeat: Infinity }}
+      >
+        <Image 
+          borderColor="whiteAlpha.200" 
+          borderWidth={2} 
+          borderStyle="solid" 
+          width="120"
+          height="120"
+          display="inline-block" 
+          borderRadius="full"
+          src="/images/mainLogo.png" 
+          alt="Logo Image" 
+        />
+      </MotionBox>
     </Box>
   )
 }
