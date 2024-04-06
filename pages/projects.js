399e2d117b5bb2399f82333b3ff3d19ee7adaa60
Andrew <Andrewyhchon@gmail.com>
2024-04-05 22:41:07 -0700

    added thumbnails into different folder

--- pages/projects.js
+++ pages/projects.js
@@ -1,10 +1,10 @@
 import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Section from '../components/section'
 import { WorkGrid } from '../components/grids'
-import thumbSlime from '../public/images/thumbnailSlime.png'
-import thumbNook from '../public/images/thumbnailNook.png'
-import thumbResearch from '../public/images/thumbnailResearch.png'
-import thumbDungeon from '../public/images/thumbnailDungeon.png'
+import thumbSlime from '../public/images/thumbnails/thumbnailSlime.png'
+import thumbNook from '../public/images/thumbnails/thumbnailNook.png'
+import thumbResearch from '../public/images/thumbnails/thumbnailResearch.png'
+import thumbDungeon from '../public/images/thumbnails/thumbnailDungeon.png'
 
 const Projects = () => (
         <Container>
